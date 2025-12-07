5dea853b621357b34b5bf17c6f6cef6ae34ff8e1
IamAhxan <mohammadahsan7744@gmail.com>
2025-12-08 00:40:56 +0500

    Create Restaurant API

--- backend/src/middleware/validation.ts
+++ backend/src/middleware/validation.ts
@@ -1,3 +1,4 @@
+import { notDeepEqual } from "assert";
 import type { NextFunction, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 
@@ -16,3 +17,21 @@ export const validateMyUserRequest = [
     body("country").isString().notEmpty().withMessage("country must be String"),
     handleValidationErrors,
 ];
+
+
+
+
+
+export const validateMyRestaurantRequest = [
+    body("restaurantName").notEmpty().withMessage("Restaurant Name is Required"),
+    body("city").notEmpty().withMessage("city Name is Required"),
+    body("country").notEmpty().withMessage("Country Name is Required"),
+    body("deliveryPrice").isFloat({ min: 0 }).withMessage("Delivery price must be a positive number"),
+    body("estimatedDelivery").isInt({ min: 0 }).withMessage("Estimated Delivery Time must be a positive Integer"),
+    body("cuisines").isArray().withMessage("Cuisines myust be an Array").not().isEmpty().withMessage("Cuisines array can not be empty"),
+    body("menuItems").isArray().withMessage("Menu Items must be an array"),
+    body("menuItems.*.name").notEmpty().withMessage("Menu Item name is required"),
+    body("menuItems.*.price").isFloat({ min: 0 }).withMessage("Menu Item Pricemust be a positive Numeber"),
+    handleValidationErrors
+
+]
---
+++ backend/src/routes/MyRestaurantRoute.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import multer from 'multer'
+import { createMyRestaurant } from '../controllers/MyRestaurantController.js'
+import { jwtCheck, jwtParse } from '../middleware/auth.js'
+import { validateMyRestaurantRequest } from '../middleware/validation.js'
+
+
+const router = express.Router()
+const storage = multer.memoryStorage()
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 2024 * 1024,
+    },
+});
+// /api/my/restaurant
+router.post("/", upload.single("imageFile"), validateMyRestaurantRequest, jwtCheck, jwtParse, createMyRestaurant)
+
+
+
+
+export default router
